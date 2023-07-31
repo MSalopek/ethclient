59a50ddf287f261d2cf3b74817211f8e37ce1ad9
MSalopek <matija.salopek994@gmail.com>
2023-07-31 11:52:59 +0200

    init: add initial commits

---
+++ cmd/fetchd/Readme.md
@@ -0,0 +1,3 @@
+# Fetchd
+
+A small program for fetching data using the geth lib to figure out how to use `ethclient` and `gethclient`.
---
+++ cmd/fetchd/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/ethclient/gethclient"
+)
+
+var apiURL = flag.String("api-url", "", "api-url of an ethereum RPC NODE")
+
+// Storage gives read access to contract internal storage
+// Storage keys cannot be iterated over in a straightforward way without knowing the contract
+// The keys (and types of keys) to check need to be known beforehand and the
+//
+// After getting storage key values, the merkle proof can be fetched using gethclient.GetProof
+func main() {
+	flag.Parse()
+	if *apiURL == "" {
+		log.Fatal("api-url is required")
+	}
+
+	client, err := ethclient.Dial(*apiURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	geth := gethclient.New(client.Client())
+
+	fmt.Println("we have a connection")
+	ctx := context.Background()
+	addr := common.HexToAddress("0xB9951B43802dCF3ef5b14567cb17adF367ed1c0F")
+	zeroHash := common.HexToHash("0x1")                           // data at slot 0
+	storageVal, err := client.StorageAt(ctx, addr, zeroHash, nil) // value is hexadecimal number
+	if err != nil {
+		fmt.Println("HAVE ERR", err)
+		return
+	}
+	fmt.Println("RESULT", storageVal)
+	padded := common.LeftPadBytes([]byte{0x0}, 32)
+	hex := common.Bytes2Hex(padded)
+	proof, err := geth.GetProof(ctx, addr, []string{hex}, nil)
+	if err != nil {
+		fmt.Println("HAVE ERR in PROOF", err)
+		return
+	}
+	buf, err := json.MarshalIndent(proof, "", "  ")
+	if err != nil {
+		fmt.Println("marshalling err", err)
+		return
+	}
+
+	fmt.Printf("PROOF\n %+v", string(buf))
+
+	// TODO: figure out how to verify using the proofs and building a trie
+
+	// the storage hash and the proof is the data to be verified
+	// storageHash := proof.StorageHash
+	// storageProof := proof.StorageProof[0]
+	// // encode the key-value pair
+	// key := common.LeftPadBytes([]byte(storageProof.Key), 32)
+	// value, err := rlp.EncodeToBytes(storageProof.Value)
+	// if err != nil {
+	// 	fmt.Println("proof err", err)
+	// 	return
+	// }
+	// build a trie with the nodes in the proof
+	// 	proofTrie := NewProofDB()
+	// 	for _, node := range storageProof.Proof {
+	// 		proofTrie.Put(crypto.Keccak256(node), node)
+	// 	}
+	// 	// verify the proof
+	// 	verified, err := VerifyProof(
+	// 		storageHash.Bytes(), crypto.Keccak256(key), proofTrie)
+	// 	require.True(t, bytes.Equal(verified, value), fmt.Sprintf("%x != %x", verified, value))
+	// }
+}
